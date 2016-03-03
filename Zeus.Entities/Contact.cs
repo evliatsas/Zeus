1f1b4a9994f12d850a8c15a1769a73fd3f2c3e72
Evangelos Liatsas <evliatsas@gmail.com>
2016-03-03 16:29:56 +0200

    Entities Update

--- Zeus.Entities/Contact.cs
+++ Zeus.Entities/Contact.cs
@@ -8,5 +8,16 @@ namespace Zeus.Entities
 {
     public class Contact : Entity
     {
+        public string Name { get; set; }
+        public string Title { get; set; }
+        public string Company { get; set; }
+        public string Address { get; set; }
+        public IList<Phone> Phones{ get; set; }
+        public string Type { get; set; }
+
+        public Contact()
+        {
+            this.Phones = new List<Phone>();
+        }
     }
 }
