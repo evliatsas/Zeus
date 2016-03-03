89c8feed9832ccca02c575358b0cd93bc8c6e651
tasos <akkgr@users.noreply.github.com>
2016-03-03 18:48:32 +0200

    server

--- Zeus/Program.cs
+++ Zeus/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Owin.Hosting;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,16 @@ namespace Zeus
     {
         static void Main(string[] args)
         {
+            using (WebApp.Start<Startup>(url: Properties.Settings.Default.ApiUrl))
+            {
+                Console.WriteLine("DDR API Server Started on {0}...", Properties.Settings.Default.ApiUrl);
+                Console.WriteLine("\n\nPress ESC to shutdown");
+                ConsoleKeyInfo key;
+                do
+                {
+                    key = Console.ReadKey(true);
+                } while (key.Key != ConsoleKey.Escape);
+            }
         }
     }
 }
---
+++ Zeus/Startup.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Owin;
+using Microsoft.Owin.Cors;
+using Owin;
+using System.Net.Http.Headers;
+using System.Web.Http;
+
+[assembly: OwinStartup(typeof(Zeus.Startup))]
+
+namespace Zeus
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            //init DB Contexts
+            var context = new Zeus.Entities.Repositories.Context();
+            
+            //Configure CORS
+            app.UseCors(CorsOptions.AllowAll);
+            //Configure OWIN
+            app.Map("/api", api =>
+            {
+                var config = new HttpConfiguration();
+
+                //Adding JSON Formatter & custom Message Handlers
+                //config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+                config.Formatters.Remove(config.Formatters.XmlFormatter);
+                //Mapping Routes and starting up WebAPI
+                config.MapHttpAttributeRoutes();
+                // require authentication for all controllers
+                config.Filters.Add(new AuthorizeAttribute());
+                api.UseWebApi(config);
+            });
+        }
+
+    }
+}
