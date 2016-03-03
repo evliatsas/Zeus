bb3cf1e3c4760970a75630cecaaf78ae1187c6e6
Evangelos Liatsas <evliatsas@gmail.com>
2016-03-03 15:00:02 +0200

    Added Entities nad NuGet

---
+++ Zeus.Entities/Entity.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zeus.Entities
+{
+    /// <summary>
+    /// Base class (abstract) of every Zeus DB Stored Type (Class)
+    /// </summary>
+    public abstract class Entity
+    {
+        /// <summary>
+        /// The Unique Identification (Primary Key) of the Entity
+        /// </summary>
+        public virtual string Id { get; set; }
+
+        /// <summary>
+        /// Data Placeholder
+        /// </summary>
+        public virtual object Tag { get; set; }
+
+        /// <summary>
+        /// Remarks for the specified Instance
+        /// </summary>
+        public virtual string Notes { get; set; }
+    }
+}
