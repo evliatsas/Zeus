4789f5a004c907ba7e2650d8e2d14821ab2c2b01
Evangelos Liatsas <evliatsas@gmail.com>
2016-03-03 13:52:25 +0200

    Initial Checkin

---
+++ Zeus.Client/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name zeusclientApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the zeusclientApp
+ */
+angular.module('zeusclientApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
