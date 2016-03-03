8f72e0175494a285399473d5079e9a8859901ec9
Evangelos Liatsas <evliatsas@gmail.com>
2016-03-03 15:59:47 +0200

    Added Facility

--- Zeus.Entities/Facility.cs
+++ Zeus.Entities/Facility.cs
@@ -8,5 +8,30 @@ namespace Zeus.Entities
 {
     public class Facility : Entity
     {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Type { get; set; }
+        public IList<Contact> Contacts { get; set; }
+        public Location Location { get; set; }
+        public IList<Person> Persons { get; set; }
+        public int Capacity { get; set; }
+        public int Attendance { get { return this.Persons.Count; } }
+        public IList<Housing> Housings { get; set; }
+        public bool IsSecure { get; set; }
+        public SecurityProvider SecurityProvider { get; set; }
+        public int FeedingCapacity { get; set; }
+        public Contact FeedingContact { get; set; }
+        public FeedingProvider FeedingProvider { get; set; }
+        public string Status { get; set; }
+        public Nullable<DateTime> StatusDateTime { get; set; }
+        public Nullable<DateTime> StatusECT { get; set; }
+        public string Administration { get; set; }
+
+        public Facility()
+        {
+            this.Contacts = new List<Contact>();
+            this.Persons = new List<Person>();
+            this.Housings = new List<Housing>();
+        }
     }
 }
