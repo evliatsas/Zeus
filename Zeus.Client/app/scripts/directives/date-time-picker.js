76564d82e7f3ec4983c6a025df1511fd77c23a8b
Antonis Katarachias <ak.hellas@gmail.com>
2016-03-05 14:38:04 +0200

    date time picker directive

---
+++ Zeus.Client/app/scripts/directives/date-time-picker.js
@@ -0,0 +1,18 @@
+﻿angular.module('zeusclientApp')
+       .directive('dateTimePicker', function ($window) {
+        return {
+            scope: {
+                label: '@',
+                placeholder: '@',
+                format: '@',
+                date: '='
+            },
+            templateUrl: '/templates/date-time-picker.html',
+            link: function postLink(scope, element, attrs) {
+                scope.opened = false;
+                scope.width = function () {
+                    return $window.innerWidth;
+                }
+            }
+        };
+});
