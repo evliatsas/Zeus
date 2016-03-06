9b77f9ba8fdff576e9a7ad174441d21f21d2ce32
tasos <akkgr@users.noreply.github.com>
2016-03-06 13:29:57 +0200

    fix map

--- Zeus.Client/app/scripts/controllers/map.js
+++ Zeus.Client/app/scripts/controllers/map.js
@@ -8,22 +8,37 @@
  * Controller of the zeusclientApp
  */
 angular.module('zeusclientApp')
-  .controller('MapCtrl', function ($http, $scope, $compile) {
-      var map;
-      map = new google.maps.Map(document.getElementById('map'), {
+  .controller('MapCtrl', function ($http, $scope, $compile, $templateRequest) {
+      
+      var map = new google.maps.Map(document.getElementById('map'), {
           center: { lat: 38.5306122, lng: 25.4556341 },
           zoom: 7
       });
+
+      var infowindow = new google.maps.InfoWindow();
+      $scope.facilities = [];
+      $scope.facility = null;
 
       $http({
           method: 'GET',
           url: 'http://localhost:8080/api/facilities'
       }).then(function successCallback(response) {
           $scope.facilities = response.data;
+          makeInfoWindow();
           $scope.facilities.forEach(addMarker);
       }, function errorCallback(response) {
           alert(response.data);
       });
+
+      function makeInfoWindow()
+      {
+          $templateRequest('/templates/facility-map.html').then(function (template) {
+              var content = $compile(template)($scope);
+              infowindow.setContent(content[0]);
+          }, function () {
+              alert("error");
+          });
+      }
             
       function addMarker(element, index, array) {
           var myLatLng = { lat: element.Location.Coordinates[0], lng: element.Location.Coordinates[1] };
@@ -31,12 +46,13 @@ angular.module('zeusclientApp')
               position: myLatLng,
               map: map,
               title: element.Name,
-              icon: "http://maps.google.com/mapfiles/ms/icons/green-dot.png"
+              icon: "http://maps.google.com/mapfiles/ms/icons/green-dot.png",
+              customInfo: element
           });
+
           marker.addListener('click', function () {
-              var infowindow = new google.maps.InfoWindow();
-              var content = '<div ng-include src="\'/templates/facility-card.html\'"></div>';
-              infowindow.setContent(content);
+              $scope.facility = marker['customInfo'];
+              $scope.$apply();
               infowindow.open(map, marker);
           });
 
@@ -50,5 +66,10 @@ angular.module('zeusclientApp')
               marker.setIcon("http://maps.google.com/mapfiles/ms/icons/blue-dot.png");
               marker.setDraggable(true);
           }
+      }
+
+      $scope.moveMarker = function()
+      {
+
       }
   });
