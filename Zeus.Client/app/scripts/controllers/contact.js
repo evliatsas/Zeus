53014573c8fe51cecee7f52140acb921ae335953
athanadrian <athanadrian@gmail.com>
2016-03-09 15:23:09 +0200

    contact

---
+++ Zeus.Client/app/scripts/controllers/contact.js
@@ -0,0 +1,81 @@
+﻿'use strict';
+
+angular
+    .module('zeusclientApp')
+    .controller('ContactCtrl', function ($scope, $http, $routeParams, $location, baseUrl, lookupService, messageService) {
+
+        var isInsert = $routeParams.id == 'new';
+
+        $scope.reportcolumns = [
+             { Caption: 'Τίτλος', Field: 'Title', Tooltip: 'Τίτλος Προμηθευτή' },
+             { Caption: 'Όνοματεπώνυμο', Field: 'Name', Tooltip: 'Όνομα Επαφής' },
+             { Caption: 'Εταιρία', Field: 'Company', Tooltip: 'Εταιρία' },
+             { Caption: 'Διαχειριστής', Field: 'Administration', Values: lookupService.administrations, Tooltip: 'Διοικητική Υπαγωγή' },
+             { Caption: 'Κατηγορία', Field: 'Type', Values: lookupService.contactTypes, Tooltip: 'Τύπος Επαφής' },
+             { Caption: 'Διεύθυνση', Field: 'Address', Tooltip: 'Διεύθυνση' },
+             { Caption: 'Τηλέφωνα', Field: 'Phones', Tooltip: 'Διοικητική Υπαγωγή' },
+             { Caption: 'Email', Field: 'Email', Tooltip: 'Email Επαφής' } 
+        ];
+
+        $http({
+            method: 'GET',
+            url: baseUrl + '/common/contacts'
+        }).then(function successCallback(response) {
+            $scope.contacts = response.data;
+        }, function errorCallback(response) {
+            messageService.getFailed(response.error);
+        });
+
+
+        if (isInsert) {
+            //$scope.data = {};
+        } else {
+            $http({
+                method: 'GET',
+                url: baseUrl + '/contacts/' + $routeParams.id
+            }).then(function successCallback(response) {
+                $scope.data = response.data;
+            }, function errorCallback(response) {
+                messageService.showError();
+            });
+        }
+
+        // SAVE - DELETE
+        $scope.save = function () {
+            if (isInsert) {
+                // Create contact
+                var method = 'POST';
+            }
+            else {
+                // Update contact
+                var method = 'PUT';
+            }
+
+            $http({
+                method: method,
+                data: $scope.data,
+                url: baseUrl + '/contacts'
+            }).then(function successCallback(response) {
+                messageService.saveSuccess();
+                $scope.data = response.data;
+            }, function errorCallback(response) {
+                messageService.showError();
+            });
+        }
+
+        var deleteItem = function () {
+            $http({
+                method: 'DELETE',
+                url: baseUrl + '/contacts/' + $scope.data.Id
+            }).then(function successCallback(response) {
+                messageService.deleteSuccess();
+                $location.url('/persons');
+            }, function errorCallback(response) {
+                messageService.showError();
+            });
+        }
+
+        $scope.delete = function () {
+            messageService.askDeleteConfirmation(deleteItem);
+        }
+    });
--- Zeus.Client/app/scripts/controllers/contacts.js
+++ Zeus.Client/app/scripts/controllers/contacts.js
@@ -2,37 +2,58 @@
 
 angular
     .module('zeusclientApp')
-    .controller('ContactsCtrl', function ($scope, $http, $location, lookupService, messageService, baseUrl) {
+    .controller('ContactsCtrl', function ($scope, $http, $routeParams, $location, lookupService, messageService, baseUrl)  {
 
-        $scope.lookup = lookupService;
-        $scope.contacts = [];
+        var isInsert = $routeParams.pid == 'new';
 
         $scope.columns = [
-             { Caption: 'Τίτλος', Field: 'Title', Tooltip: 'Τίτλος Προμηθευτή' },
-             { Caption: 'Όνομα', Field: 'Name', Tooltip: 'Όνομα Επαφής' },
-             { Caption: 'Εταιρία', Field: 'Company', Tooltip: 'Εταιρία' },
-             { Caption: 'Υπαγωγή', Field: 'Administration', Values: lookupService.administrations, Tooltip: 'Διοικητική Υπαγωγή' },
-             { Caption: 'Τύπος', Field: 'Type', Values: lookupService.contactTypes, Tooltip: 'Τύπος Επαφής' },
-             { Caption: 'Διεύθυνση', Field: 'Address', Tooltip: 'Διεύθυνση' },
-             { Caption: 'Υπαγωγή', Field: 'Administration', Tooltip: 'Διοικητική Υπαγωγή' },
-             { Caption: 'Email', Field: 'Email', Tooltip: 'Email Επαφής' }             
+            { Caption: 'Τίτλος', Field: 'Title', Tooltip: 'Τίτλος Προμηθευτή' },
+            { Caption: 'Όνομα', Field: 'Name', Tooltip: 'Όνομα Επαφής' },
+            { Caption: 'Εταιρία', Field: 'Company', Tooltip: 'Εταιρία' },
+            { Caption: 'Υπαγωγή', Field: 'Administration', Values: lookupService.administrations, Tooltip: 'Διοικητική Υπαγωγή' },
+            { Caption: 'Τύπος', Field: 'Type', Values: lookupService.contactTypes, Tooltip: 'Τύπος Επαφής' },
+            { Caption: 'Διεύθυνση', Field: 'Address', Tooltip: 'Διεύθυνση' },
+            { Caption: 'Υπαγωγή', Field: 'Administration', Tooltip: 'Διοικητική Υπαγωγή' },
+            { Caption: 'Email', Field: 'Email', Tooltip: 'Email Επαφής' }
         ];
 
-        $scope.addItem = function () {
-            $location.url('/contacts/new');
+        $scope.lookup = lookupService;
+        $scope.contact = {};
+
+        $scope.addContact = function () {
+            $location.url("/contacts/new");
         }
 
-        $scope.openItem = function (contact) {
-            $location.url('/contacts/' + contact.Id);
-        }
+        //if (!isInsert) {
+        //    $http({
+        //        method: 'GET',
+        //        url: baseUrl + '/contacts/' + $routeParams.pid //the unique id of the provider
+        //    }).then(function successCallback(response) {
+        //        $scope.provider = response.data;
+        //    }, function errorCallback(response) {
+        //        messageService.showError();
+        //    });
+        //}
+        //else {
+        //    $scope.contact = {};
+        //    $scope.contacts.Items = [];
+        //}
 
-        $http({
-            method: 'GET',
-            url: baseUrl + '/contacts'
-        }).then(function successCallback(response) {
-            $scope.contacts = response.data;
-        }, function errorCallback(response) {
-            messageService.getFailed(response.error);
-        });
+        //$scope.addItem = function () {
+        //    $location.url('/contacts/new');
+        //}
+
+        //$scope.openItem = function (contact) {
+        //    $location.url('/contacts/' + contact.Id);
+        //}
+
+        //$http({
+        //    method: 'GET',
+        //    url: baseUrl + '/contacts'
+        //}).then(function successCallback(response) {
+        //    $scope.contacts = response.data;
+        //}, function errorCallback(response) {
+        //    messageService.getFailed(response.error);
+        //});
 
     });
