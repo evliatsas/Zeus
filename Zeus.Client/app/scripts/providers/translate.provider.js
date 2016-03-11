36accaa72b0720bd2e40520c6d3146f122804d71
athanadrian <athanadrian@gmail.com>
2016-03-11 16:35:02 +0200

    facility

--- Zeus.Client/app/scripts/providers/translate.provider.js
+++ Zeus.Client/app/scripts/providers/translate.provider.js
@@ -8,22 +8,22 @@ angular
 	    	MENU: {
 	    		BRAND: 'ξ ε ν ί α',
 	    		FACILITIES: 'Δομές Φιλοξενίας',
-	    		CONTACTS: 'contacts',
-	    		PROVIDERS: 'providers',
-	    		PERSONS: 'persons',
-	    		MAP: 'map',
-	    		CHARTS: 'charts',
-	    		USERS: 'users',
-	    		ARCHIVES: 'archives',
+	    		CONTACTS: 'Επαφές',
+	    		PROVIDERS: 'Υποστήριξη',
+	    		PERSONS: 'Ταυτοποιημένοι',
+	    		MAP: 'Χάρτης',
+	    		CHARTS: 'Στατιστικά',
+	    		USERS: 'Χρήστες',
+	    		ARCHIVES: 'Αρχείο',
 	    		SUPPORT: {
-	    			HELP: 'help',
-	    			ABOUT: 'about'
+	    			HELP: 'Βοήθεια',
+	    			ABOUT: 'Για την Εφαρμογή'
 	    		},
 	    		USER: {
-	    			SIGN_IN: 'sign in',
-	    			CHANGE_PASSWORD: 'change password',
-	    			MESSAGES: 'messages',
-	    			SIGN_OUT: 'sign out'
+	    			SIGN_IN: 'Σύνδεση',
+	    			CHANGE_PASSWORD: 'Αλλαγή Κωδικού',
+	    			MESSAGES: 'Μηνύματα',
+	    			SIGN_OUT: 'Αποσύνδεση'
 	    		}
 	    	},
 	    	FACILITY: {
@@ -31,16 +31,41 @@ angular
 	    	    SAVE: 'Αποθήκευση Προμηθευτή'
 	    	},
 	    	FACILITYDETAILS: {
-	    	    NAME: 'Δομή Φιλοξενίας :',
+	    	    NAME: 'Δομή Φιλοξενίας: ',
 	    	    DESCRIPTION:'Περιγραφή',
 	    	    TYPE: 'Κατηγορία',
 	    	    CAPACITY: 'Χωρητικότητα',
-	    	    ATTENDANCE: 'Φιλοξενούμενοι :',
+	    	    ATTENDANCE: 'Φιλοξενούμενοι: ',
 	    	    ISSECURE: 'Φυλασσόμενος Χώρος',
 	    	    HASHEALTHCARE: 'Υγεινομική Υποστήριξη',
 	    	    STATUS: 'Κατάσταση',
 	    	    ADMINISTRATION: 'Διαχειριστής',
-	    	    NOTES: 'Κείμενο'
+	    	    NOTES: 'Κείμενο',
+	    	    STATUSDATETIME: 'Ημερομηνία Κατάστασης',
+	    	    STATUSECT: 'Εκτιμώμενη Ημερομηνία Αποκατάστασης'
+	    	},
+	    	FACILITYCARD: {
+	    	    GUESTS: 'Φιλοξενούμενοι: ',
+	    	    REPORTS: 'Αναφορές: ',
+	    	    IDENTIFIED: 'Ταυτοποιημένοι: ',
+	    	    APPLICATION: 'Αίτηση',
+	    	    HOUSING: 'Στέγαση: ',
+	    	    TRANSPORT: 'Μετακίνηση',
+	    	    SITUATION: 'Κατάσταση',
+	    	    PROBLEM: 'Πρόβλημα',
+	    	    MESSAGE: 'Μήνυμα'
+	    	},
+	    	FACILITYMAP: {
+	    	    GUESTS: 'Φιλοξενούμενοι: ',
+	    	    CAPACITY: 'Χωρητικότητα',
+	    	    UTILIZATION: 'Πληρότητα: ',
+	    	    TRANSITION: 'Μετάβαση',
+	    	    SAVE: 'Αποθήκευση: ',
+	    	    MOVE: 'Μετακίνηση'
+	    	},
+	    	LOOKUPMODAL: {
+	    	    CANCEL: 'ΑΚΥΡΩΣΗ: ',
+	    	    SELECT: 'ΕΠΙΛΟΓΗ'
 	    	}
 	    };
 
