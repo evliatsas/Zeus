684c6e86a6ff04ef9dbdc0b711062df6de0817b9
tasos <akkgr@users.noreply.github.com>
2016-03-12 11:12:04 +0200

    fixed index

--- Zeus.Client/app/scripts/controllers/menu.js
+++ Zeus.Client/app/scripts/controllers/menu.js
@@ -2,7 +2,10 @@
 
 angular
     .module('zeusclientApp')
-    .controller('MenuCtrl', function($scope, $location, authService) {
+    .controller('MenuCtrl', function($scope, $location, $http, baseUrl, authService) {
+
+        $scope.isAuth = authService.isAuth();
+
         $scope.getClass = function(path) {
             if ($location.path() === path) {
                 return 'active';
@@ -11,16 +14,21 @@ angular
             }
         }
 
-        $scope.isAuth = function () {
-            return authService.isAuth();
-        }
-
-        $scope.logOut = function () {
+        $scope.logOut = function() {
             authService.logout();
             $location.url("/login");
         }
 
-        $scope.changePassword = function (password,newPassword,passwordConfirm) {
+        $scope.changePassword = function(password, newPassword, passwordConfirm) {
             authService.changePassword("", password, newPassword, passwordConfirm);
         }
+
+        $http({
+            method: 'GET',
+            url: baseUrl + '/reports/message/unread'
+        }).then(function successCallback(response) {
+            $scope.unread = response.data;
+        }, function errorCallback(response) {
+            $scope.unread = 0;
+        });
     });
