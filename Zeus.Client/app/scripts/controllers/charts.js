3e858923d6d395bd94a715a846ea1c2e36c4d53c
Antonis Katarachias <ak.hellas@gmail.com>
2016-03-12 18:26:55 +0200

    charts updates

--- Zeus.Client/app/scripts/controllers/charts.js
+++ Zeus.Client/app/scripts/controllers/charts.js
@@ -2,7 +2,7 @@
 
 angular
     .module('zeusclientApp')
-    .controller('ChartsCtrl', function($scope, $http, $filter, baseUrl, messageService, lookupService) {
+    .controller('ChartsCtrl', function ($scope, $http, $filter, baseUrl, messageService, lookupService) {
 
         $scope.lookup = lookupService;
 
@@ -14,30 +14,50 @@ angular
         $http({
             method: 'GET',
             url: baseUrl + '/reports/facilities/stats'
-        }).then(function successCallback(response) {
+        }).then(function successCallback (response) {
             $scope.reports = response.data;
-            $scope.reports.forEach(function(item, index) {
+            $scope.reports.forEach(function (item, index) {
                 item.Checked = false;
 
-                var type = $filter('filter')($scope.types, function(c) { return c == item.Type; })[0];
+                var type = $filter('filter')($scope.types, function (c) { return c == item.Type; })[0];
                 if (type == null) { $scope.types.push(item.Type); }
 
-                var facility = $filter('filter')($scope.facilities, function(f) { return f.Id == item.Facility.Id; })[0];
-                if (facility == null) { $scope.facilities.push(item.Facility); }
-
-                generateDataForSitReps($scope.reports);
+                var facility = $filter('filter')($scope.facilities, function (f) { return f.Id == item.Facility.Id; })[0];
+                if (facility == null) {
+                    $scope.facilities.push(item.Facility);
+                    $scope.facilities[$scope.facilities.length - 1].Checked = true;
+                }
             });
 
-        }, function errorCallback(response) {
+        }, function errorCallback (response) {
             messageService.getFailed(response.error);
         });
 
-        $scope.getType = function(type) {
-            return $filter('filter')($scope.lookup.reports, function(r) { return r.Id == type; })[0].Description;
+        $scope.getType = function (type) {
+            return $filter('filter')($scope.lookup.reports, function (r) { return r.Id == type; })[0].Description;
+        }
+
+        $scope.$watch('reports', function (newValue, oldValue) {
+
+        });
+
+        var notReady = true;
+        var filtered = [];
+
+        $scope.update = function (type, facility) {
+            if (notReady == true) { return; }
+            filtered = [];
+            $scope.reports.forEach(function (report, index) {
+                var facility = $filter('filter')($scope.facilities, function (f) { return f.Name == report.Facility.Name; })[0];
+                if (facility != null && facility.Checked == true) {
+                    filtered.push(report);
+                }
+            });
+            generateDataForSitReps(filtered, "DD-MM");
         }
 
         function format(dt, format) {
-            var format = format || "D-M";
+            var format = format || "DD-MM";
             return moment(dt).isValid() ? moment(dt).format(format, 'el') : "";
         }
 
@@ -45,11 +65,12 @@ angular
             var groups = [];
             if (items == null) { return groups; }
 
-            items.forEach(function (item, index) {
+            items.forEach(function(item, index) {
 
                 var key = typeof groupBy === "function" ? groupBy(item) : item[groupBy];
 
-                var group = $filter('filter')(groups, function (g) { return g.key == key; })[0];
+                var group = $filter('filter')(groups, function(g) {
+                    return g.key == key; })[0];
 
                 if (group == null) {
                     group = {
@@ -76,22 +97,20 @@ angular
             };
 
             var labels = groupBy(reports, labelsFn);
-            var empty = labels.map(function (l) { return 0; });
+            var empty = labels.map(function(l) { return null; });
 
             var series = groupBy(reports, seriesFn);
 
-                series.forEach(function (serie, index) {
-                    chart.series.push(serie.key);
-                    var row = empty.slice();
-                    chart.data.push(row);
-                });
+            series.forEach(function(serie, index) {
+                chart.series.push(serie.key);
+                var row = empty.slice();
+                chart.data.push(row);
+            });
 
-            labels.forEach(function (label, index) {
+            labels.forEach(function(label, index) {
                 chart.labels.push(label.key);
 
-
-
-                label.items.forEach(function (report, index) {
+                label.items.forEach(function(report, index) {
                     var s = chart.series.indexOf(seriesFn(report));
                     var l = chart.labels.indexOf(labelsFn(report));
                     chart.data[s][l] = dataFn(report);
@@ -106,14 +125,15 @@ angular
         function generateDataForSitReps(reports, dateFormat) {
             if (reports == null) { return; }
 
-            var labelsFn = function (report) { return format(report.DateTime, dateFormat); }
-            var seriesFn = function (report) { return report.Facility.Name; }
-            var dataFn = function (report) { return report.PersonCount; }
+            var labelsFn = function(report) { return format(report.DateTime, dateFormat); }
+            var seriesFn = function(report) { return report.Facility.Name; }
+            var dataFn = function(report) { return report.PersonCount; }
 
             return generateReportStats(reports, labelsFn, seriesFn, dataFn);
         }
 
-        // $scope.$on('ngRepeatFinished', function(ngRepeatFinishedEvent) {
-        //     $scope.charts = [];
-        // });
+        $scope.$on('ngRepeatFinished', function(ngRepeatFinishedEvent) {
+            notReady = false;
+            generateDataForSitReps($scope.reports, "DD-MM");
+        });
     });
