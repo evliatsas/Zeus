fb2554489d287133828af276caa593b66a551577
tasos <akkgr@users.noreply.github.com>
2016-03-12 18:51:30 +0200

    fixed users

---
+++ Zeus.Web/Models/RegisterUser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Zeus.Models
+{
+    public class RegisterUser : ApplicationUser
+    {
+        public string Password { get; set; }
+        public string NewPassword { get; set; }
+        public string PasswordConfirm { get; set; }
+    }
+}
