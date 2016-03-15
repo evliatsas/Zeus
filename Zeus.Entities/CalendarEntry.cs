e0c3c5b14d491da382467ada0b1ba8e665f6c223
Evangelos Liatsas <evliatsas@gmail.com>
2016-03-15 16:18:26 +0200

    added calendar entry

---
+++ Zeus.Entities/CalendarEntry.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zeus.Entities
+{
+    public class CalendarEntry : Entity
+    {
+        public string SourceId { get; set; }
+        public DateTime DateTime { get; set; }
+        public string Author { get; set; }
+        public string Description { get; set; }
+        public string Actions { get; set; }
+
+        public CalendarEntry()
+        {
+            this.DateTime = DateTime.Now;
+        }
+
+        public CalendarEntry(Report report, string author)
+        {
+            this.SourceId = report.Id;
+            this.DateTime = report.DateTime;
+            this.Author = author;
+            this.Description = String.Format("{0}\n{1}\n{2}", report.Facility.Name, report.Subject, report.Notes);
+        }
+
+        public CalendarEntry(Operation operation)
+        {
+            this.SourceId = operation.Id;
+            this.DateTime = DateTime.Now;
+            this.Author = "Κέντρο Επιχειρήσεων";
+            this.Description = String.Format("{0}\n{1}", operation.Name, operation.ReportedProblems);
+        }
+    }
+}
