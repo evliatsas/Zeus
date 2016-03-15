e088d55e09cb442fe2f77f3b935073a1f9ef1990
athanadrian <athanadrian@gmail.com>
2016-03-15 16:19:40 +0200

    calendar

--- Zeus.Client/app/scripts/controllers/calendar.js
+++ Zeus.Client/app/scripts/controllers/calendar.js
@@ -2,6 +2,17 @@
 
 angular
     .module('zeusclientApp')
-    .controller('CalendarCtrl', function () {
+    .controller('CalendarCtrl', function ($scope, $http, $location, lookupService, messageService, baseUrl) {
 
+        $scope.lookup = lookupService;
+        $scope.calendar = [];
+
+        $scope.calendarcolumns = [
+             { Caption: 'GRID.START', Field: 'Start', Type: 'DateTime', Tooltip: 'Ώρα' },
+             { Caption: 'GRID.TYPE', Field: 'Type', Type: 'Lookup', Values: lookupService.administrations, Tooltip: 'Φορέας' },
+             { Caption: 'GRID.PRIORITY', Field: 'Priority', Type: 'LookupHtml', Values: lookupService.priorities, Tooltip: 'Προτεραιότητα Αναφοράς' },
+             { Caption: 'GRID.SUBJECT', Field: 'Περιγραφή Αναφοράς' },
+             { Caption: 'GRID.ACTIONS', Field: 'Actions', Tooltip: 'Πλήθος Ατόμων' },
+             { Caption: 'GRID.ISARCHIVE', Field: 'IsArchive', Type: 'Boolean', Tooltip: 'Έχει Ολοκληρωθεί η Προετοιμασία' },
+        ];
     });
