e293750cdc312314c4df4378d5cafe56de859bda
Evangelos Liatsas <evliatsas@gmail.com>
2016-03-16 20:10:06 +0200

    fixed csproj

---
+++ Zeus.Entities/Reports/HealthcareProblemReport.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zeus.Entities
+{
+    public class HealthcareProblemReport : Report
+    {
+        public string Sortages { get; set; }
+
+        public HealthcareProblemReport()
+        {
+            this.DateTime = DateTime.Now;
+            this.Type = ReportType.HealthcareProblemReport;
+        }
+    }
+}
