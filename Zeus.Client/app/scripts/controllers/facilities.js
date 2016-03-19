a35d16bdae37b66a2d85c07edec1b65c05dd589e
Evangelos Liatsas <evliatsas@gmail.com>
2016-03-19 11:36:21 +0200

    added facilities totals

--- Zeus.Client/app/scripts/controllers/facilities.js
+++ Zeus.Client/app/scripts/controllers/facilities.js
@@ -9,6 +9,7 @@ angular
             url: baseUrl + '/facilities'
         }).then(function successCallback(response) {
             $scope.facilities = response.data;
+            calcTotals();
         }, function errorCallback(response) {
             messageService.getFailed(response.error);
         });
@@ -19,5 +20,26 @@ angular
 
         $scope.addFacility = function () {
             $location.url("/facilities/new");
+        }
+
+        $scope.attendanceClass = function (facility) {
+            if (facility.Attendance >= facility.Capacity)
+                return 'text-danger';
+            else if (facility.Attendance >= (0.75 * facility.Capacity))
+                return 'text-warning';
+            else return 'text-success';
+        }
+
+        $scope.totalAttendance = 0;
+        $scope.totalCapacity = 0;
+        $scope.totalArrivals = 0;
+        $scope.totalRations = 0;
+        var calcTotals = function () {
+            angular.forEach($scope.facilities, function (value, key) {
+                $scope.totalAttendance += value.Attendance;
+                $scope.totalCapacity += value.Capacity;
+                $scope.totalArrivals += value.Arrivals;
+                $scope.totalRations += value.MaxRations;
+            });
         }
     });
